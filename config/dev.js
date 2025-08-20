7f31d565113bb6f1bc4e7834fe966716eecaa737
Adi <dihash@gmail.com>
2025-08-20 17:21:05 +0300

    Build front

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,5 @@
 export default {
-    dbURL: 'mongodb://127.0.0.1:27017',
+    // dbURL: 'mongodb://127.0.0.1:27017',
+    dbURL: 'mongodb+srv://adi:51290@cluster0.xeqzr2q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     dbName: 'toyDB',
 }
